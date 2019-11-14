fe8b45e6b17afd2b32cec7cb4ffabf93e4416438
jaroslawmichalik <michalik@protonmail.ch>
2019-11-14 15:15:46 +0100

    Remove -E flag

--- make-test-pass.sh
+++ make-test-pass.sh
@@ -1,1 +1,1 @@
-find src/test/kotlin/tech/michalik/flowriddles -type f -name "Riddle[0-9]*Test.kt" -exec sed -i -E "s/Riddle\([0-9]*\)\.solve/tech.michalik.flowriddles.solutions.Riddle\1Solution\.solve/" {} \;
+find src/test/kotlin/tech/michalik/flowriddles -type f -name "Riddle[0-9]*Test.kt" -exec sed -i "s/Riddle\([0-9]*\)\.solve/tech.michalik.flowriddles.solutions.Riddle\1Solution\.solve/" {} \;
