0270675916e77dca9caed29bbd5f64c290687bf3
jaroslawmichalik <michalik@protonmail.ch>
2019-11-17 18:38:08 +0100

    Adjust codestyle

--- src/main/kotlin/tech/michalik/flowriddles/Riddle12.kt
+++ src/main/kotlin/tech/michalik/flowriddles/Riddle12.kt
@@ -4,7 +4,8 @@ import kotlinx.coroutines.flow.Flow
 
 object Riddle12 {
     /**
-     * Let the first emission of the [source] within a time window of 300ms travel downstream but don't emit any other events until the next time window.
+     * Let the first emission of the [source] within a time window of 300ms travel downstream
+     * but don't emit any other events until the next time window.
      * Use case: prevent double click on button
      */
     fun solve(source: Flow<Unit>): Flow<Unit> {
--- src/main/kotlin/tech/michalik/flowriddles/solutions/Riddle12Solution.kt
+++ src/main/kotlin/tech/michalik/flowriddles/solutions/Riddle12Solution.kt
@@ -5,7 +5,8 @@ import kotlinx.coroutines.flow.sample
 
 object Riddle12Solution {
     /**
-     * Let the first emission of the [source] within a time window of 300ms travel downstream but don't emit any other events until the next time window.
+     * Let the first emission of the [source] within a time window of 300ms travel downstream
+     * but don't emit any other events until the next time window.
      * Use case: prevent double click on button
      */
     fun solve(source: Flow<Unit>): Flow<Unit> {
